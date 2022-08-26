c4877b490fc445bf72e9cf27660edbeab06538bd
lyh031026huahuo1234 <112098685+lyh031026huahuo1234@users.noreply.github.com>
2022-08-27 00:02:13 +0800

    new python

--- animal.py
+++ animal.py
@@ -1,10 +1,17 @@
 import sys
 
+def cat():
+	print("cat")
+
+
 def default():
 	print("animal")
 
 def main():
-	default()
+	if sys.argv[1] == 'cat' :
+		cat()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
