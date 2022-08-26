b1d6deaaaa5ab1772f6f6a6056e0d7d0f5819f43
lyh031026huahuo1234 <112098685+lyh031026huahuo1234@users.noreply.github.com>
2022-08-27 00:10:17 +0800

    add dog function

--- animal.py
+++ animal.py
@@ -1,10 +1,16 @@
 import sys
 
+def dog():
+	print("dog!")
+
 def default():
 	print("animal")
 
 def main():
-	default()
+	if sys.argv[1] == 'dog':
+		dog()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
